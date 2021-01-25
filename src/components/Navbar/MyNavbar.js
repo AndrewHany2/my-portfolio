6630bbf1bd4fc0fcd5035dea5ec3cb6c2b0fae5a
Andrew Hany <andrewhany260@gmail.com>
2021-01-25 23:31:33 +0200

    animate navbar

--- src/components/Navbar/MyNavbar.js
+++ src/components/Navbar/MyNavbar.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from "react";
 import { SidebarData } from "./SidebarData";
 import { Link } from "react-scroll";
 import { Nav, Navbar } from "react-bootstrap";
+import Animate from "react-smooth";
 import "./Navbar.css";
 
 function MyNavbar(props) {
@@ -21,33 +22,35 @@ function MyNavbar(props) {
   });
   return (
     <>
-      <Navbar
-        className={navbar ? "navb navbar-active" : "navb"}
-        fixed="top"
-        bg="dark"
-        variant="dark"
-        expand="md"
-      >
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="navb">
-            {SidebarData.map((item, index) => (
-              <Link
-                key={index}
-                to={item.path}
-                spy={true}
-                smooth={true}
-                duration={800}
-              >
-                <Nav.Link className="navbar-item mx-2 px-2">
-                  {item.icon}
-                  <span className="ml-2">{item.title}</span>
-                </Nav.Link>
-              </Link>
-            ))}
-          </Nav>
-        </Navbar.Collapse>
-      </Navbar>
+      <Animate to="1" from="0" attributeName="opacity">
+        <Navbar
+          className={navbar ? "navb navbar-active" : "navb"}
+          fixed="top"
+          bg="dark"
+          variant="dark"
+          expand="md"
+        >
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="navb">
+              {SidebarData.map((item, index) => (
+                <Link
+                  key={index}
+                  to={item.path}
+                  spy={true}
+                  smooth={true}
+                  duration={800}
+                >
+                  <Nav.Link className="navbar-item mx-2 px-2">
+                    {item.icon}
+                    <span className="ml-2">{item.title}</span>
+                  </Nav.Link>
+                </Link>
+              ))}
+            </Nav>
+          </Navbar.Collapse>
+        </Navbar>
+      </Animate>
       {/* ) : (
         <nav className={props.sidebar ? "nav-menu active-nav" : "nav-menu"}>
           <ul className="nav-menu-items">
